3efbd96b46b214c36dd10cb426e29efcec7be65f
Arkadiusz Rogacki <arek.rogacki@gmail.com>
2024-06-18 17:20:10 +0200

    cleanup

--- src/main/java/congestion/calculator/congestiontaxcalculator/enums/Vehicle.java
+++ src/main/java/congestion/calculator/congestiontaxcalculator/enums/Vehicle.java
@@ -1,7 +1,5 @@
 package congestion.calculator.congestiontaxcalculator.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 import java.util.EnumSet;
 
 public enum Vehicle {
@@ -16,11 +14,6 @@ public enum Vehicle {
     private static final EnumSet<Vehicle> TOLL_FREE = EnumSet.of(
         MOTORCYCLE, BUS, EMERGENCY, DIPLOMAT, FOREIGN, MILITARY
     );
-
-    @JsonCreator
-    public static Vehicle fromString(String key) {
-        return Vehicle.valueOf(key.toUpperCase());
-    }
 
     public boolean isTollFree() {
         return TOLL_FREE.contains(this);
